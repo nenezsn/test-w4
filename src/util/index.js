4066d0744f6ad31247ecf905402ef0b7471ad5bd
王冰 <wangbinge@seentao.com>
2020-04-13 16:05:16 +0800

    use babel7 / add-module-exports 解决require 引用export default模块需要添加 .default 问题／@babel指的是babel8

--- src/util/index.js
+++ src/util/index.js
@@ -1,6 +1,9 @@
-export function add(a,b){
+ function add(a,b){
     return '这是加法'
 }
-export function increase(a,b){
+ function increase(a,b){
     return '这是减法'
+}
+export default{
+    add,increase
 }
