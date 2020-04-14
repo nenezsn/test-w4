3177e9037d67ece8c28b7767aaf24dd30caaf0b5
王冰 <wangbinge@seentao.com>
2020-04-14 08:49:37 +0800

    use terser-webpack-plugin and thread-loader 多进程构建

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,20 @@
 const webpackMerge = require('webpack-merge')
+const TerserPlugin = require('terser-webpack-plugin');
+const path = require('path')
 var common = require('./webpack.base.js')
-module.exports = webpackMerge(common,{
-    mode:'production'
+
+const baseUrl = 'https://pbu-public.oss-cn-beijing.aliyuncs.com/webapps/pbu_portal/1585028489957/dist/'
+
+module.exports = webpackMerge(common, {
+    output: {
+        filename: 'index.js',
+        path: path.join(__dirname, 'dist'),
+        publicPath: baseUrl
+    },
+    optimization:{
+        minimizer:[
+            new TerserPlugin()
+        ]
+    },
+    mode: 'production'
 })
