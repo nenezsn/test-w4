d3c6e2d7f9c754f8379db48948a53842c57dc8e4
王冰 <wangbinge@seentao.com>
2020-06-08 14:07:11 +0800

    suspense React.lazy

---
+++ src/components/public/pdf.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Pdf extends Component {
+    render() {
+        return (
+            <div>
+                pdf
+            </div>
+        );
+    }
+}
+
+export default Pdf;
--- src/route/test.js
+++ src/route/test.js
@@ -1,6 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component,Suspense } from 'react';
+const LazyComponent = React.lazy(()=>import('../components/public/pdf'))
 
 class test extends Component {
+    state={
+        show:false
+    }
     getLazy=()=>{
         import('../util/index').then(data=>{
             console.log('data',data.default)
@@ -12,11 +16,22 @@ class test extends Component {
            console.log('data',data)
         },'util')
     }
+    getLazy3=()=>{
+        this.setState({
+            show:true
+        })
+    }
     render() {
         return (
             <div>
                 <button onClick={this.getLazy}>import懒加载</button>
                 <button onClick={this.getLazy2}>require.ensure懒加载</button>
+                <button onClick={this.getLazy3}>React.Lazy懒加载</button>
+                {this.state.show ? 
+                <Suspense fallback={<div>loading</div>}>
+                    <LazyComponent/>
+                </Suspense>
+                : null }
             </div>
         );
     }
