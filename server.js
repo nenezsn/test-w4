837813dc3e02e6185f8936bc964d3f5f70eb30f9
王冰 <wangbinge@seentao.com>
2020-06-10 11:23:35 +0800

    封装 pbu_http_proxy

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const compression = require('compression')
+const request = require('request');
 
 const app = express()
 app.set('port', 3000)
@@ -15,6 +16,42 @@ app.use(bodyParser.urlencoded({ extended: true })) //解析 Content-type : appli
 app.use(cookieParser())
 
 app.use(express.static('./dist'))
+
+function pbuServerRequest(url,form){
+    return new Promise((resolve,reject)=>{
+        request.post({
+            url,
+            form
+        },(err, response, body)=>{
+            if (err) {
+                return reject(err);
+            }
+            const ret = JSON.parse(body);
+            resolve(ret);
+        })
+    })
+}
+
+function pbu_http_proxy(config,logger){
+    return function(req,res,next){
+        if(req.method != 'POST'){
+            return next()
+        }
+        const path = req.path.split('/').reverse();
+        const url = `${config.businessApiUrl}/${path[1]}/${path[0]}`
+        const body = req.body
+        if(logger){console.log('request:::::',url,body)}
+        pbuServerRequest(url,body).then(data=>{
+        if(logger){console.log('responce:::::',data)}
+            res.json(data)
+        }).catch(err=>{
+        if(logger){console.log('error:::::',err)}
+            res.json(err)
+        })
+    }
+}
+
+app.use(pbu_http_proxy({businessApiUrl:'https://tapi.seentao.com'},true))
 
 app.get('*', (req, res, next) => {
     res.status(200).sendFile(path.resolve(__dirname + '/dist/index.html'))
