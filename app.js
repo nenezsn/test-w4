81c2a273892a397e7b3b04ce2a0f3c9cdbb76bb2
王冰 <wangbinge@seentao.com>
2020-06-10 11:23:08 +0800

    app.js监听服务状态

---
+++ app.js
@@ -0,0 +1,13 @@
+module.exports = app => {
+  app.name='egg'
+  app.once('server', server => {
+    console.log('app',app.config.env)
+    console.log('server is runing in localhost:7002')
+  });
+  app.on('error', (err, ctx) => {
+  });
+  app.on('request', ctx => {
+  });
+  app.on('response', ctx => {
+  });
+};
