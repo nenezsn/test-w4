5274331fa148b3d47143997981bd04bd0388462e
王冰 <wangbinge@seentao.com>
2020-06-10 14:40:06 +0800

    实现上传功能

--- app/controller/upload.js
+++ app/controller/upload.js
@@ -6,11 +6,18 @@ class upload extends Controller {
   async upload() {
     const { ctx } = this;
     const file = ctx.request.files[0];
-    const name = 'egg-multipart-test/' + path.basename(file.filename);
+   console.log('文件信息',file);
     fs.readFile(file.filepath, (err, data) => {
-        console.log('data',data)
-        fs.writeFile('../upload/' + file.filename, data, err => {
+        fs.writeFile(path.resolve(__dirname,'../upload') +'/'+ file.filename, data, err => {
             console.log('err',err)
+            if(!err){
+              console.log('上传成功')
+              fs.unlink(file.filepath,(err,data)=>{
+                if(!err){
+                  console.log('删除成功')
+                }
+              });
+            }
         })
     })
     ctx.body = {
--- app/service/public.js
+++ app/service/public.js
@@ -2,7 +2,7 @@ const Service  = require('egg').Service
 
 class Public extends Service{
     async getAppUrl(){
-    return this.app.config.news
+    return this.config.news
     }
 }
 module.exports = Public
