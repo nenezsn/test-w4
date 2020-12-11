63bac84e3fd86660d0483d8467f341e298591eb8
王冰 <wangbinge@seentao.com>
2020-12-11 13:57:09 +0800

    关闭反向代理是 socket.js hostname检查报错

--- webpack.config.js
+++ webpack.config.js
@@ -7,11 +7,12 @@ module.exports = webpackMerge(common, {
     mode: 'development',
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
-        port: '8000',
+        port: '3001',
         hot: true,
         open: true,
         compress: true,
         historyApiFallback: true,
+        disableHostCheck: true, //  新增该配置项 解决反向代理报错
         proxy: proxy
     },
 })
